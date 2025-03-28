7bde544de31eaf57d31f82c1bef19a64f1d0d088
Rodrigo1999 <rodrigosousa1999rss@gmail.com>
2025-03-28 13:13:16 -0300

    fix(useDebounceChangeFieldHandler): did`t update field when is changed from form.methods.setValue
    example: when call form.methods.setValue(fieldName, value) the component input not updated because the hook not watched changes like this

--- src/hooks/useDebounceChangeFieldHandler.ts
+++ src/hooks/useDebounceChangeFieldHandler.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { FieldValues, UseControllerReturn } from "react-hook-form";
 import { debounce } from "../utils";
 import { getEventValue } from "../utils/getEventValue";
@@ -10,12 +10,20 @@ export function useDebounceChangeFieldHandler(controller: UseControllerReturn<Fi
     const debounceSubmitDefinitions = useNiceformHookContext(state => state.form.control.debounceSubmitDefinitions)
     const onChangeDebounce = useMemo(() => debounce(controller.field.onChange, time), [time, controller.field.onChange])
     const newOnChange = useCallback((evt) => {
-        setValue(getEventValue(evt))
-        onChangeDebounce(evt)
+        const value = getEventValue(evt)
+        setValue(value)
+        onChangeDebounce(value)
         debounceSubmitDefinitions.set(time)
     }, [setValue, onChangeDebounce, time])
 
     if(!enabled) return;
+    
+    const controlledValue = controller.field.value
+    useEffect(() => {
+        if(!Object.is(value, controlledValue)){
+            setValue(controlledValue)
+        }
+    }, [controlledValue])
     
     controller.field.onChange = newOnChange
     controller.field.value = value
