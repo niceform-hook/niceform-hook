048998dc17f6fd42b8535734cee8874d3bb685ff
Rodrigo1999 <rodrigosousa1999rss@gmail.com>
2025-08-20 15:14:24 -0300

    fix: fix two problem
    1 - the debounce function was recreating itself, the memorization was not configured well.
    
    2 - correction in the detection of field value changes from external control, what was configured conflicted with the debounce.

--- src/hooks/useDebounceChangeFieldHandler.ts
+++ src/hooks/useDebounceChangeFieldHandler.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { FieldValues, UseControllerReturn } from "react-hook-form";
 import { debounce } from "../utils";
 import { getEventValue } from "../utils/getEventValue";
@@ -6,25 +6,34 @@ import { useNiceformHookContext } from "../contexts/niceform";
 
 export function useDebounceChangeFieldHandler(controller: UseControllerReturn<FieldValues, string>, time = 400, enabled = true){
     const [value, setValue] = useState<any>(controller.field.value)
+    const fieldOnchange = useRef(controller.field.onChange)
+    const canExternalUpdate = useRef(true)
 
     const debounceSubmitDefinitions = useNiceformHookContext(state => state.form.control.debounceSubmitDefinitions)
-    const onChangeDebounce = useMemo(() => debounce(controller.field.onChange, time), [time, controller.field.onChange])
+
+    const onChangeDebounce = useMemo(() => debounce((...args) => {
+        canExternalUpdate.current = true
+        fieldOnchange.current(...args)
+    }, time), [time])
+
     const newOnChange = useCallback((evt) => {
+        canExternalUpdate.current = false
         const value = getEventValue(evt)
         setValue(value)
         onChangeDebounce(value)
         debounceSubmitDefinitions.set(time)
     }, [setValue, onChangeDebounce, time])
 
-    if(!enabled) return;
-    
     const controlledValue = controller.field.value
     useEffect(() => {
-        if(!Object.is(value, controlledValue)){
+        if(canExternalUpdate.current && !Object.is(value, controlledValue)){
             setValue(controlledValue)
+            controller.field.value = controlledValue
         }
     }, [controlledValue])
     
+    if(!enabled) return;
+
     controller.field.onChange = newOnChange
     controller.field.value = value
 }
